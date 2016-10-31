dd641b243a22c35128cedb57ae73db0e13853101
kateduquesnay <13khd@queensu.ca>
2016-10-31 09:46:49 -0400

    Create Qu4.sh

---
+++ Qu4.sh
@@ -0,0 +1,8 @@
+#Assignment 2
+#Sarah Carter 10193595
+#Katherine DuQuesnay 10138898
+#Adele McCallum 10093867
+#Dean Wilkins-Reeves 10176758
+# git clone https://github.com/kateduquesnay/cisc220_2.git cisc220_2
+
+#Question 4

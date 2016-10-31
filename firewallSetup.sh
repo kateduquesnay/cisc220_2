5af8e49d0cfeb59bb5ef4fe3bd880b7805c48a35
Dean Wilkins-Reeves <14drwr@queensu.ca>
2016-10-31 14:02:35 -0700

    added top comments

--- firewallSetup.sh
+++ firewallSetup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#Assignment 2
+#Sarah Carter 10193595
+#Adele McCallum 10093867
+#Dean Wilkins-Reeves 19176758
+#Katherine DuQuesnay 10138898
+#git clone https://github.com/kateduquesnay/cisc220_2.git cisc220_2
+
 #get rid of previous rules
 sudo iptables -F
 sudo iptables -X

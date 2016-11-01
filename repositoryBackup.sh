05845b5551b5bd4851393a7671ec5afbf45daa13
AdeleMcCallum <12adm10@queensu.ca>
2016-10-31 22:56:22 -0400

    Update repositoryBackup.sh

--- repositoryBackup.sh
+++ repositoryBackup.sh
@@ -9,8 +9,33 @@
 
 
 #repositoryBackup.sh
-#Clone a repository: git clone <Your Remote Repository URL> <Name of your local folder>
+#read 
 
-#git clone https://github.com/kateduquesnay/cisc220_2 Assignment 2
 
-#cron job: 00 1 * * * ~do the thing~
+#cron job:
+
+#in loop?
+crontab -e
+00 1 * * * git clone https://github.com/kateduquesnay/cisc220_2 Assignment 2
+#name the file based on the date: backupYYMMDDHH.tgz
+
+#import the path to the repository folder: 
+#
+#import the github URL:
+#read https://github.com/kateduquesnay/cisc220_2 ??
+#import git username and password 
+
+#create a date and time variable based on when the cron job runs??? for naming??
+
+#Commit the new backup file to the local git repository 
+  #git clone "https://github.com/kateduquesnay/cisc220_2"
+  #cd git-backup
+#Push the new backup file to your remote git repository 
+git push https://AdeleMcCallum:jenny1234@github.com/repoURL.git --all
+  
+
+#output redirected to the backupsLog file
+#echo Backup (backupYYYYMMDDHH.tgz) created successfully!
+#echo Backup (backupYYYYMMDDHH.tgz) committed to the local git repository
+#echo Backup (backupYYYYMMDDHH.tgz) pushed to the remote git repository cisc220_2
+
